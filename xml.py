c8ee93552461370b80b7e6d7d39fc0ba31cf00a3
Gustavo Tironi <126670916+gtironi@users.noreply.github.com>
2023-04-29 13:51:28 +0000

    Começo do for

--- xml.py
+++ xml.py
@@ -3,30 +3,19 @@ from urllib.request import urlopen
 import xml.etree.ElementTree as ET
 import ssl
 
-api_key = False
-# If you have a Google Places API key, enter it here
-# api_key = 'AIzaSy___IDByT70'
-# https://developers.google.com/maps/documentation/geocoding/intro
-
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter location: ')
-
 print('Retrieving', url)
 data = urlopen(url, context=ctx).read()
 
-    data = uh.read()
-    print('Retrieved', len(data), 'characters')
-    print(data.decode())
-    tree = ET.fromstring(data)
+print('Retrieved', len(data), 'characters')
 
-    results = tree.findall('result')
-    lat = results[0].find('geometry').find('location').find('lat').text
-    lng = results[0].find('geometry').find('location').find('lng').text
-    location = results[0].find('formatted_address').text
+tree = ET.fromstring(data)
+counts = tree.findall('.//count')
 
-    print('lat', lat, 'lng', lng)
-    print(location)
+for count in counts:
+    

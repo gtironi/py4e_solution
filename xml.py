d0053393ceaa0ed13ec95e14ca73c33ced8b77c4
Gustavo Tironi <126670916+gtironi@users.noreply.github.com>
2023-04-29 13:42:26 +0000

    Mudança total no codigo

--- xml.py
+++ xml.py
@@ -1,4 +1,5 @@
 import urllib.request, urllib.parse, urllib.error
+from urllib.request import urlopen
 import xml.etree.ElementTree as ET
 import ssl
 
@@ -7,27 +8,15 @@ api_key = False
 # api_key = 'AIzaSy___IDByT70'
 # https://developers.google.com/maps/documentation/geocoding/intro
 
-if api_key is False:
-    api_key = 42
-    serviceurl = 'http://py4e-data.dr-chuck.net/xml?'
-else :
-    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/xml?'
-
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-while True:
-    address = input('Enter location: ')
-    if len(address) < 1: break
+url = input('Enter location: ')
 
-    parms = dict()
-    parms['address'] = address
-    if api_key is not False: parms['key'] = api_key
-    url = serviceurl + urllib.parse.urlencode(parms)
-    print('Retrieving', url)
-    uh = urllib.request.urlopen(url, context=ctx)
+print('Retrieving', url)
+data = urlopen(url, context=ctx).read()
 
     data = uh.read()
     print('Retrieved', len(data), 'characters')
